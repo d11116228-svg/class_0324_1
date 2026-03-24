970a9581acc0dbbbe98ccf185d3b410de523ba30
d11116228-svg <d11116228@gs.takming.edu.tw>
2026-03-24 13:54:59 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Class_0324_1
+{
+    public class Test
+    {
+        private int Mid;
+        private const int Final = 99;
+
+        // 1 個參考
+        public int MyMid { get; } = 56;
+
+        // 2 個參考
+        public int MyFinal
+        {
+            get
+            {
+                return Final;
+            }
+            set
+            {
+                if (value > 0 && value < 100)
+                {
+                    //Final = value;
+                }
+                else
+                {
+                    //Final = 0;
+                }
+            }
+        }
+    }
